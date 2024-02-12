53260251570a407e5831d70707499e18051035ba
Fabse <93054078+fabse-hack@users.noreply.github.com>
2024-02-12 19:59:46 +0100

    Update discord-notification-script.js

--- .github/workflows/discord-notification-script.js
+++ .github/workflows/discord-notification-script.js
@@ -1,22 +1,20 @@
 const { Client } = require('discord.js');
-
 const client = new Client();
-const githubRepo = 'https://github.com/fabse-hack/water.rocket/';
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
-
-client.on('message', async message => {
-  if (message.content === '!watchCommits') {
-    message.channel.send('Ich werde jetzt Commits überwachen.');
-
-    const channel = client.channels.cache.find(ch => ch.name === 'rocketscience');
-
-    setInterval(() => {
-      channel.send(`Es wurde ein neuer Commit in ${githubRepo} gemacht!`);
-    }, 60000);
-  }
+client.once('ready', () => {
+    console.log('Bot is ready!');
 });
 
 client.login(process.env.BOT_KEY);
+
+client.on('message', async message => {
+    if (message.content === '!watchCommits') {
+        message.channel.send('Ich werde jetzt Commits überwachen.');
+
+        const channel = client.channels.cache.find(ch => ch.name === 'rocketscience');
+
+        setInterval(() => {
+            channel.send('Es wurde ein neuer Commit gemacht!');
+        }, 60000); // Überprüfe alle 60 Sekunden auf neue Commits
+    }
+});
