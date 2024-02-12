d7f6ba6c19c448fd15a6b6c251503152e2baf252
Fabse <93054078+fabse-hack@users.noreply.github.com>
2024-02-12 19:35:04 +0100

    Create discord-notification-script.js

---
+++ .github/workflows/discord-notification-script.js
@@ -0,0 +1,22 @@
+const { Client } = require('discord.js');
+
+const client = new Client();
+const githubRepo = 'https://github.com/fabse-hack/water.rocket';
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', async message => {
+  if (message.content === '!watchCommits') { 
+    message.channel.send('Ich werde jetzt Commits überwachen.');
+
+    const channel = client.channels.cache.find(ch => ch.name === 'rocketscience');
+
+    setInterval(() => {
+      channel.send(`Es wurde ein neuer Commit in ${githubRepo} gemacht!`);
+    }, 60000);
+  }
+});
+
+client.login('process.env.BOT_KEY);
