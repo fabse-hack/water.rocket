a73b23f09c684c206b6a8a753a0975db4bb9890f
Fabse <93054078+fabse-hack@users.noreply.github.com>
2024-02-26 21:42:22 +0100

    Update send_discord_message.py

--- .github/workflows/send_discord_message.py
+++ .github/workflows/send_discord_message.py
@@ -4,31 +4,23 @@ import asyncio
 
 async def send_discord_message(message_content):
     token = os.environ.get("DISCORD_TOKEN")
-    if not token:
-        print("Fehler: Discord-Token nicht gefunden.")
-        return
-
     target_channel_id = '1145979711097426001'
 
     client = discord.Client()
 
     @client.event
     async def on_ready():
-        print(f"Eingeloggt als {client.user}")
 
         channel = client.get_channel(int(target_channel_id))
-        if channel:
-            await channel.send(message_content)
-            print("Nachricht erfolgreich gesendet.")
-        else:
-            print("Fehler: Kanal nicht gefunden.")
+        channel:
+          await channel.send(message_content)
 
         await client.close()
 
-    await client.start(token)  # Starte den Client hier
+    await client.start(token)
 
 async def main():
-    message_content = "Dies ist eine Testnachricht von GitHub Actions!"
+    message_content = "Ein neuer Commit ist eingegangen auf https://github.com/fabse-hack/water.rocket"
     await send_discord_message(message_content)
     
 asyncio.run(main())
